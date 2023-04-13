e4c290e9a821a9cc6cdae16d966ede5e4bb50a87
mioriaty <mioriaty@gmail.com>
2023-04-13 18:11:54 +0700

    updating

--- src/app.js
+++ src/app.js
@@ -8,10 +8,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const partner_url = "https://pokeapi.co/api/v2/pokemon";
+const partner_url = "https://randomuser.me/api/?results=50";
 const btn = document.querySelector("#test-button");
 const body = document.body;
 const loader = document.getElementById("loading-spinner");
+const sectionCard = document.getElementById("section-card");
+const checkboxGroup = Array.from(document.querySelectorAll(".checkbox-container input[type='checkbox']"));
 class Store {
     getPartnerApps({ onFailure, onSuccess, onLoading }) {
         return __awaiter(this, void 0, void 0, function* () {
@@ -24,14 +26,90 @@ class Store {
             }
             catch (error) {
                 onFailure === null || onFailure === void 0 ? void 0 : onFailure(error);
+                throw new Error("Failed");
             }
         });
     }
 }
+function handleRedirect(link) {
+    window.open(link, "_blank");
+}
+class ViewController {
+    constructor() {
+        this.renderData = () => {
+            return this.data.map((item) => {
+                const card = this.renderItem(item);
+                sectionCard.innerHTML += `${card}`;
+            });
+        };
+        this.renderItem = (item) => {
+            console.log(this.filters);
+            return /* html */ `
+      <div data-gender="${item.gender}" class="col-md-4">
+        <div class="card">
+          <!-- card head -->
+          <div class="_card-header">
+            <div class="_card-header__image">
+              <img class="img-fluid" src="${item.picture.medium}" />
+            </div>
+
+            <div>
+              <button 
+                data-id="${item.name.first}-${item.name.last}" 
+                type="button" 
+                class="_card-header__button" 
+                onclick="handleRedirect('${item.picture.large}')">
+                  Explore More</button>
+            </div>
+
+          </div>
+
+          <!-- card body -->
+          <div class="_card-body">
+            <div class="_card-body__title">
+              ${item.gender}
+            </div>
+            <div class="_card-body__description">
+              ${item.name.first} ${item.name.last}
+            </div>
+          </div>
+
+          <!-- card footer -->
+          <div class="_card-footer">
+            ${item.email}
+          </div>
+        </div>
+      </div>
+    `;
+        };
+        this.handleCheckbox = () => {
+            checkboxGroup.forEach((item) => {
+                item.addEventListener("change", (event) => {
+                    const inputElement = event.target;
+                    const checked = inputElement.checked;
+                    if (checked) {
+                        this.filters.push(inputElement.name);
+                    }
+                    else {
+                        this.filters.filter((name) => name !== inputElement.name);
+                    }
+                });
+            });
+        };
+        this.data = [];
+        this.filters = [];
+    }
+    init(data) {
+        this.data = data;
+        this.renderData();
+        this.handleCheckbox();
+    }
+}
 const store = new Store();
+const view = new ViewController();
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield store.getPartnerApps({
+        const data = yield store.getPartnerApps({
             onLoading() {
                 loader.classList.add("display");
             },
@@ -39,6 +117,7 @@ function main() {
                 loader.classList.remove("display");
             },
         });
+        view.init(data.results);
     });
 }
 main();
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,34 @@
-const partner_url = "https://pokeapi.co/api/v2/pokemon";
+const partner_url = "https://randomuser.me/api/?results=50";
 const btn = document.querySelector("#test-button") as HTMLButtonElement;
 const body = document.body;
 const loader = document.getElementById("loading-spinner") as HTMLElement;
+const sectionCard = document.getElementById("section-card") as HTMLElement;
+const checkboxGroup = Array.from(
+  document.querySelectorAll(".checkbox-container input[type='checkbox']")
+) as HTMLInputElement[];
+
+interface ResponseData {
+  results: Result[];
+}
+
+interface Result {
+  id: {
+    name: string;
+    value?: string;
+  };
+  gender: string;
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  email: string;
+  picture: {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  };
+}
 
 interface AsyncListener {
   onLoading?: () => void;
@@ -14,18 +41,104 @@ class Store {
     onLoading?.();
     try {
       const response: Response = await fetch(partner_url);
-      const data = await response.json();
+      const data: ResponseData = await response.json();
       onSuccess?.();
       return data;
     } catch (error) {
       onFailure?.(error as Error);
+      throw new Error("Failed");
     }
   }
 }
+
+function handleRedirect(link: string) {
+  window.open(link, "_blank");
+}
+
+class ViewController {
+  private data: Result[];
+  private filters: string[];
+
+  constructor() {
+    this.data = [];
+    this.filters = [];
+  }
+
+  renderData = () => {
+    return this.data.map((item) => {
+      const card = this.renderItem(item);
+      sectionCard.innerHTML += `${card}`;
+    });
+  };
+
+  renderItem = (item: Result) => {
+    console.log(this.filters);
+    return /* html */ `
+      <div data-gender="${item.gender}" class="col-md-4">
+        <div class="card">
+          <!-- card head -->
+          <div class="_card-header">
+            <div class="_card-header__image">
+              <img class="img-fluid" src="${item.picture.medium}" />
+            </div>
+
+            <div>
+              <button 
+                data-id="${item.name.first}-${item.name.last}" 
+                type="button" 
+                class="_card-header__button" 
+                onclick="handleRedirect('${item.picture.large}')">
+                  Explore More</button>
+            </div>
+
+          </div>
+
+          <!-- card body -->
+          <div class="_card-body">
+            <div class="_card-body__title">
+              ${item.gender}
+            </div>
+            <div class="_card-body__description">
+              ${item.name.first} ${item.name.last}
+            </div>
+          </div>
+
+          <!-- card footer -->
+          <div class="_card-footer">
+            ${item.email}
+          </div>
+        </div>
+      </div>
+    `;
+  };
+
+  handleCheckbox = () => {
+    checkboxGroup.forEach((item) => {
+      item.addEventListener("change", (event) => {
+        const inputElement = event.target as HTMLInputElement;
+        const checked = inputElement.checked;
+
+        if (checked) {
+          this.filters.push(inputElement.name);
+        } else {
+          this.filters.filter((name) => name !== inputElement.name);
+        }
+      });
+    });
+  };
+
+  init(data: Result[]) {
+    this.data = data;
+    this.renderData();
+    this.handleCheckbox();
+  }
+}
+
 const store = new Store();
+const view = new ViewController();
 
 async function main() {
-  await store.getPartnerApps({
+  const data = await store.getPartnerApps({
     onLoading() {
       loader.classList.add("display");
     },
@@ -33,6 +146,7 @@ async function main() {
       loader.classList.remove("display");
     },
   });
+  view.init(data.results);
 }
 
 main();
