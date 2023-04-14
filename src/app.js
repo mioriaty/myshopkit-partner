c9eeb545cf607720c9926fdf9f4146cc76b77cde
mioriaty <mioriaty@gmail.com>
2023-04-14 14:29:46 +0700

    done

--- src/app.js
+++ src/app.js
@@ -1,123 +1,65 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-const partner_url = "https://randomuser.me/api/?results=50";
-const btn = document.querySelector("#test-button");
-const body = document.body;
-const loader = document.getElementById("loading-spinner");
 const sectionCard = document.getElementById("section-card");
 const checkboxGroup = Array.from(document.querySelectorAll(".checkbox-container input[type='checkbox']"));
-class Store {
-    getPartnerApps({ onFailure, onSuccess, onLoading }) {
-        return __awaiter(this, void 0, void 0, function* () {
-            onLoading === null || onLoading === void 0 ? void 0 : onLoading();
-            try {
-                const response = yield fetch(partner_url);
-                const data = yield response.json();
-                onSuccess === null || onSuccess === void 0 ? void 0 : onSuccess();
-                return data;
-            }
-            catch (error) {
-                onFailure === null || onFailure === void 0 ? void 0 : onFailure(error);
-                throw new Error("Failed");
-            }
-        });
-    }
+const inputSearch = document.getElementById("search-input");
+const clearButton = document.querySelector(".button-reset");
+const listApps = Array.from(document.querySelectorAll("#section-card .card-app"));
+function debounce(callback, waitFor) {
+    let timeout = 0;
+    return (...args) => {
+        let result;
+        clearTimeout(timeout);
+        timeout = window.setTimeout(() => {
+            result = callback(...args);
+        }, waitFor);
+        return result;
+    };
 }
 function handleRedirect(link) {
     window.open(link, "_blank");
 }
-class ViewController {
-    constructor() {
-        this.renderData = () => {
-            return this.data.map((item) => {
-                const card = this.renderItem(item);
-                sectionCard.innerHTML += `${card}`;
-            });
-        };
-        this.renderItem = (item) => {
-            console.log(this.filters);
-            return /* html */ `
-      <div data-gender="${item.gender}" class="col-md-4">
-        <div class="card">
-          <!-- card head -->
-          <div class="_card-header">
-            <div class="_card-header__image">
-              <img class="img-fluid" src="${item.picture.medium}" />
-            </div>
-
-            <div>
-              <button 
-                data-id="${item.name.first}-${item.name.last}" 
-                type="button" 
-                class="_card-header__button" 
-                onclick="handleRedirect('${item.picture.large}')">
-                  Explore More</button>
-            </div>
-
-          </div>
-
-          <!-- card body -->
-          <div class="_card-body">
-            <div class="_card-body__title">
-              ${item.gender}
-            </div>
-            <div class="_card-body__description">
-              ${item.name.first} ${item.name.last}
-            </div>
-          </div>
-
-          <!-- card footer -->
-          <div class="_card-footer">
-            ${item.email}
-          </div>
-        </div>
-      </div>
-    `;
-        };
-        this.handleCheckbox = () => {
-            checkboxGroup.forEach((item) => {
-                item.addEventListener("change", (event) => {
-                    const inputElement = event.target;
-                    const checked = inputElement.checked;
-                    if (checked) {
-                        this.filters.push(inputElement.name);
-                    }
-                    else {
-                        this.filters.filter((name) => name !== inputElement.name);
-                    }
-                });
-            });
-        };
-        this.data = [];
-        this.filters = [];
-    }
-    init(data) {
-        this.data = data;
-        this.renderData();
-        this.handleCheckbox();
-    }
-}
-const store = new Store();
-const view = new ViewController();
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const data = yield store.getPartnerApps({
-            onLoading() {
-                loader.classList.add("display");
-            },
-            onSuccess() {
-                loader.classList.remove("display");
-            },
-        });
-        view.init(data.results);
+const checkValueIncudesInArray = (arrStr, str) => arrStr.some((item) => item.toLowerCase() === str.toLowerCase());
+let searchValue = "";
+let checkboxValues = [];
+function filterCards(searchVal, checkboxVal) {
+    listApps.forEach((card) => {
+        var _a, _b;
+        const cardSearchValue = ((_a = card.getAttribute("data-search")) === null || _a === void 0 ? void 0 : _a.toLowerCase()) || "";
+        const cardCategoryValue = ((_b = card.getAttribute("data-category")) === null || _b === void 0 ? void 0 : _b.toLowerCase()) || "";
+        const hasSearchMatch = searchVal
+            ? cardSearchValue.includes(searchVal)
+            : true;
+        const hasCheckboxMatch = checkboxVal.length === 0 || checkboxVal.includes(cardCategoryValue);
+        if (hasSearchMatch && hasCheckboxMatch) {
+            card.classList.add("show");
+            card.classList.remove("hide");
+        }
+        else {
+            card.classList.add("hide");
+            card.classList.remove("show");
+        }
     });
 }
-main();
+inputSearch.addEventListener("input", debounce((event) => {
+    const target = event.target;
+    searchValue = target.value;
+    filterCards(searchValue, checkboxValues);
+}, 200));
+checkboxGroup.forEach((item) => {
+    item.addEventListener("change", (event) => {
+        const inputElement = event.target;
+        const checked = inputElement.checked;
+        if (checked) {
+            checkboxValues = [...checkboxValues, inputElement.value];
+        }
+        else {
+            checkboxValues = checkboxValues.filter((value) => value !== inputElement.value);
+        }
+        filterCards(searchValue, checkboxValues);
+    });
+});
+clearButton.addEventListener("click", () => {
+    inputSearch.value = "";
+    checkboxGroup.forEach((item) => (item.checked = false));
+    filterCards("", []);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,152 +1,96 @@
-const partner_url = "https://randomuser.me/api/?results=50";
-const btn = document.querySelector("#test-button") as HTMLButtonElement;
-const body = document.body;
-const loader = document.getElementById("loading-spinner") as HTMLElement;
 const sectionCard = document.getElementById("section-card") as HTMLElement;
+
 const checkboxGroup = Array.from(
   document.querySelectorAll(".checkbox-container input[type='checkbox']")
 ) as HTMLInputElement[];
+const inputSearch = document.getElementById("search-input") as HTMLInputElement;
 
-interface ResponseData {
-  results: Result[];
-}
+const clearButton = document.querySelector(
+  ".button-reset"
+) as HTMLButtonElement;
 
-interface Result {
-  id: {
-    name: string;
-    value?: string;
+const listApps = Array.from(
+  document.querySelectorAll("#section-card .card-app")
+);
+
+function debounce<T extends (...args: any[]) => any>(
+  callback: T,
+  waitFor: number
+) {
+  let timeout = 0;
+  return (...args: Parameters<T>): ReturnType<T> => {
+    let result: any;
+    clearTimeout(timeout);
+    timeout = window.setTimeout(() => {
+      result = callback(...args);
+    }, waitFor);
+
+    return result;
   };
-  gender: string;
-  name: {
-    title: string;
-    first: string;
-    last: string;
-  };
-  email: string;
-  picture: {
-    large: string;
-    medium: string;
-    thumbnail: string;
-  };
-}
-
-interface AsyncListener {
-  onLoading?: () => void;
-  onSuccess?: () => void;
-  onFailure?: (error: Error) => void;
-}
-
-class Store {
-  async getPartnerApps({ onFailure, onSuccess, onLoading }: AsyncListener) {
-    onLoading?.();
-    try {
-      const response: Response = await fetch(partner_url);
-      const data: ResponseData = await response.json();
-      onSuccess?.();
-      return data;
-    } catch (error) {
-      onFailure?.(error as Error);
-      throw new Error("Failed");
-    }
-  }
 }
 
 function handleRedirect(link: string) {
   window.open(link, "_blank");
 }
 
-class ViewController {
-  private data: Result[];
-  private filters: string[];
+const checkValueIncudesInArray = (arrStr: string[], str: string) =>
+  arrStr.some((item) => item.toLowerCase() === str.toLowerCase());
 
-  constructor() {
-    this.data = [];
-    this.filters = [];
-  }
+let searchValue = "";
+let checkboxValues: string[] = [];
 
-  renderData = () => {
-    return this.data.map((item) => {
-      const card = this.renderItem(item);
-      sectionCard.innerHTML += `${card}`;
-    });
-  };
+function filterCards(searchVal: string, checkboxVal: string[]) {
+  listApps.forEach((card) => {
+    const cardSearchValue =
+      card.getAttribute("data-search")?.toLowerCase() || "";
+    const cardCategoryValue =
+      card.getAttribute("data-category")?.toLowerCase() || "";
 
-  renderItem = (item: Result) => {
-    console.log(this.filters);
-    return /* html */ `
-      <div data-gender="${item.gender}" class="col-md-4">
-        <div class="card">
-          <!-- card head -->
-          <div class="_card-header">
-            <div class="_card-header__image">
-              <img class="img-fluid" src="${item.picture.medium}" />
-            </div>
-
-            <div>
-              <button 
-                data-id="${item.name.first}-${item.name.last}" 
-                type="button" 
-                class="_card-header__button" 
-                onclick="handleRedirect('${item.picture.large}')">
-                  Explore More</button>
-            </div>
-
-          </div>
-
-          <!-- card body -->
-          <div class="_card-body">
-            <div class="_card-body__title">
-              ${item.gender}
-            </div>
-            <div class="_card-body__description">
-              ${item.name.first} ${item.name.last}
-            </div>
-          </div>
-
-          <!-- card footer -->
-          <div class="_card-footer">
-            ${item.email}
-          </div>
-        </div>
-      </div>
-    `;
-  };
-
-  handleCheckbox = () => {
-    checkboxGroup.forEach((item) => {
-      item.addEventListener("change", (event) => {
-        const inputElement = event.target as HTMLInputElement;
-        const checked = inputElement.checked;
-
-        if (checked) {
-          this.filters.push(inputElement.name);
-        } else {
-          this.filters.filter((name) => name !== inputElement.name);
-        }
-      });
-    });
-  };
-
-  init(data: Result[]) {
-    this.data = data;
-    this.renderData();
-    this.handleCheckbox();
-  }
-}
-
-const store = new Store();
-const view = new ViewController();
-
-async function main() {
-  const data = await store.getPartnerApps({
-    onLoading() {
-      loader.classList.add("display");
-    },
-    onSuccess() {
-      loader.classList.remove("display");
-    },
+    const hasSearchMatch = searchVal
+      ? cardSearchValue.includes(searchVal)
+      : true;
+    const hasCheckboxMatch =
+      checkboxVal.length === 0 || checkboxVal.includes(cardCategoryValue);
+    if (hasSearchMatch && hasCheckboxMatch) {
+      card.classList.add("show");
+      card.classList.remove("hide");
+    } else {
+      card.classList.add("hide");
+      card.classList.remove("show");
+    }
   });
-  view.init(data.results);
 }
 
-main();
+inputSearch.addEventListener(
+  "input",
+  debounce((event) => {
+    const target = event.target as HTMLInputElement;
+    searchValue = target.value;
+
+    filterCards(searchValue, checkboxValues);
+  }, 200)
+);
+
+checkboxGroup.forEach((item) => {
+  item.addEventListener("change", (event) => {
+    const inputElement = event.target as HTMLInputElement;
+    const checked = inputElement.checked;
+
+    if (checked) {
+      checkboxValues = [...checkboxValues, inputElement.value];
+    } else {
+      checkboxValues = checkboxValues.filter(
+        (value) => value !== inputElement.value
+      );
+    }
+
+    filterCards(searchValue, checkboxValues);
+  });
+});
+
+clearButton.addEventListener("click", () => {
+  inputSearch.value = "";
+  checkboxGroup.forEach((item) => (item.checked = false));
+
+  filterCards("", []);
+});
